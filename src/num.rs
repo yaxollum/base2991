1e763c4a748645b98d619e10dab565acbf348c37
Peter Ye <yaxollum@gmail.com>
2022-03-03 17:20:39 -0500

    implement fractions

--- src/num.rs
+++ src/num.rs
@@ -55,10 +55,15 @@ impl Num {
             'b' => Some(2),
             _ => None,
         }?;
-        Some(Self {
-            base,
-            val: u64::from_str_radix(&s[..s.len() - 1], base).ok()? as f64,
-        })
+        let val = if let Some((a_str, b_str)) = s.split_once('.') {
+            let b_str = &b_str[..b_str.len() - 1];
+            let a = u64::from_str_radix(a_str, base).ok()? as f64;
+            let b = u64::from_str_radix(b_str, base).ok()? as f64;
+            a + 1.0 / (base as f64).powi(b_str.len() as i32) * b
+        } else {
+            u64::from_str_radix(&s[..s.len() - 1], base).ok()? as f64
+        };
+        Some(Self { base, val })
     }
     pub fn convert_base(&self, new_base: u32) -> Self {
         Self {
